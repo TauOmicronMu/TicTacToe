d8dcdd95ac6b01318fcb52d2cd63ec7120f651f3
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-02-05 14:20:47 +0000

    Commented some files more thoroughly

--- src/Client.java
+++ src/Client.java
@@ -23,18 +23,27 @@ public class Client {
 		ObjectInputStream fromServer = null;
 		ObjectOutputStream toServer = null;
 		//Attempt to open an input and output stream to/from the server.
+		/*
+		 * ALWAYS OPEN OUTPUT BEFORE INPUT BECAUSE IT WON'T WORK OTHERWISE,
+		 * BECAUSE JAVA HAS REALLY WEIRD QUIRKS.
+		 */
 		try {
 			System.out.println("Started opening I/O Streams.");
+		
 			toServer = new ObjectOutputStream(socket.getOutputStream());
 			System.out.println("Opened ObjectOutputStream");
+			
 			fromServer = new ObjectInputStream(socket.getInputStream());
 			System.out.println("Opened ObjectInputStream");
+			
 			toServer.flush();
 		}
 		catch (Exception e){
 		    Constants.errorAndEnd("Something went wrong opening and flushing the I/O Stream.");
 		}
 		
+		//Debugging
+		/*
 		try {
 			byte[] hello = new byte[5];
 			fromServer.read(hello);
@@ -42,6 +51,7 @@ public class Client {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		*/
 		
 		//Attempt to send our nickname to the server. The server will respond to us, telling us what our nickname is.
 		try {
@@ -52,7 +62,10 @@ public class Client {
 			Constants.errorAndEnd("Something went wrong sending the nickname to the server.");
 		}
 		
-		//TODO : create a thread to constantly get input to the server - do everything else directly in here,
+		//TODO : create a thread to constantly get and interpret input from the server - do everything else directly in here,
 		//       there is no need for 2 threads.
+        
+		//TODO : Display a JList that holds all client names with a busy state of false.
+		//TODO : Make the JList elements selectable.
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -9,6 +9,7 @@ public class Server {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		//Create a new ConnectedClientData
 		ConnectedClientData connectedClients = new ConnectedClientData();
+		
 		//Attempt to open a ServerSocket on port Constants.PORT.
 		ServerSocket serverSocket = null;
 		try {
@@ -22,24 +23,38 @@ public class Server {
 		while(true) {
 			Socket client = serverSocket.accept();
 		    System.out.println("Client connected.");
-			System.out.println("Client : " + client);
+			
+		    //Debugging 
+		    //System.out.println("Client : " + client);
+			
 		    //Grab the I/O Streams from the client.
 			ObjectInputStream fromClient = null;
 			ObjectOutputStream toClient = null;
 		    
+			/*
+			 * ALWAYS OPEN OUTPUT BEFORE INPUT BECAUSE IT WON'T WORK OTHERWISE,
+			 * BECAUSE JAVA HAS REALLY WEIRD QUIRKS.
+			 */
 			try {
 				System.out.println("Started trying to get IO streams.");
+				
 				toClient = new ObjectOutputStream(client.getOutputStream());
 			    System.out.println("Got ObjectOutputStream.");
+			    
 				fromClient = new ObjectInputStream(client.getInputStream());
 			    System.out.println("Got ObjectInputStream.");
-			    toClient.write("Hello".getBytes());
-			    toClient.flush();
+			    
+			    /*
+			     * Debugging
+			     */
+			    //toClient.write("Hello".getBytes());
+			    //toClient.flush();
 			}
 			catch (Exception e) {
 				Constants.errorAndEnd("Something went wrong opening and flushing the streams to/from the client.");
 			}
 			
+			//Try reading the client's nickname from the input stream.
 			String clientName = null;
 			try {
 			    clientName = (String) fromClient.readObject();
@@ -49,10 +64,15 @@ public class Server {
 			    Constants.errorAndEnd("Error reading clientName in Server - not a string.");
 			}
 
+			//Allocate a unique username to the client.
 			String finalClientName = Constants.getUnusedUsername(clientName, connectedClients);
 			
-			System.out.println("Final client name : " + finalClientName);
+			//TODO : Echo this username back to the client.
 			
+			//Debugging
+			//System.out.println("Final client name : " + finalClientName);
+			
+			//Create a new record in connectedClients with the new client's name.
 			connectedClients.addNewClient(finalClientName);
 		}
  	}	
